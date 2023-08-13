75bfa723ab6470d2eeefb18ec9ffe7daf09ef75e
samuel <samuel.baker@rack-a-tiers.com>
2023-08-13 12:01:55 -0700

    - added login page and link on index.html

---
+++ my-app/src/login.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+  
+    loginForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+  
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+  
+      // Set your default username and password
+      const defaultUsername = "admin";
+      const defaultPassword = "password";
+  
+      if (username === defaultUsername && password === defaultPassword) {
+        alert("Login successful!");
+        window.location.href = "main.html"; // Redirect to the main app page after successful login.
+      } else {
+        alert("Invalid username or password. Please try again.");
+      }
+    });
+  });
+  

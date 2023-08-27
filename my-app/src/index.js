bc6379405cd90843aea1b3228cd87f09c3bd77e9
samuel <samuel.baker@rack-a-tiers.com>
2023-08-26 21:51:05 -0700

    - added electron-refresh for easier development

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const electronReload = require('electron-reload');
 const path = require('path');
 
 
@@ -31,8 +32,7 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
+app.on('ready', createWindow, electronReload(__dirname));
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.

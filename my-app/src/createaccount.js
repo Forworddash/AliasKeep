a51e86234f33643f6c6819358571b10729d7611e
samuel <samuel.baker@rack-a-tiers.com>
2023-08-27 18:25:26 -0700

    - added account creation and login with creds

--- my-app/src/createaccount.js
+++ my-app/src/createaccount.js
@@ -4,25 +4,29 @@ document.addEventListener("DOMContentLoaded", function () {
     createaccountForm.addEventListener("submit", function (event) {
       event.preventDefault();
   
-      const username = document.getElementById("username").value;
-      const password = document.getElementById("password").value;
+      const createdUsername = document.getElementById("username").value;
+      const createdPassword = document.getElementById("password").value;
       const password2 = document.getElementById("password2").value;
       let account = false; // using let to make variable mutable
 
       // Match both passwords and set account to true if they match and username and password are not empty
-      if (password === password2 && username != "" && password != "") {
+      if (createdPassword === password2 && createdUsername != "" && createdPassword != "") {
         alert("Account created succesfully, redirecting to login page!");
         account = true;
+
+        // Store username and password in local storage
+        localStorage.setItem("createdUsername", createdUsername);
+        localStorage.setItem("createdPassword", createdPassword);
         window.location.href = "login.html"; // Redirect to the login page after successful account creation.
       } else {
         alert("Passwords do not match. Please try again.");
       }
     });
-    
+
       // go to login page if cancel button is pressed
       const cancelButton = document.getElementById("cancel");
       cancelButton.addEventListener("click", function (event) {
       event.preventDefault();
       window.location.href = "login.html";
       });
-  });
+});
--- my-app/src/login.js
+++ my-app/src/login.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loginForm = document.getElementById("login-form");
   
+    
+    // Retrieve stored username and password from local storage
+    const storedUsername = localStorage.getItem("createdUsername");
+    const storedPassword = localStorage.getItem("createdPassword");
+    // Set your default username and password
+    const defaultUsername = "admin";
+    const defaultPassword = "password";
+    
     loginForm.addEventListener("submit", function (event) {
       event.preventDefault();
   
       const username = document.getElementById("username").value;
       const password = document.getElementById("password").value;
   
-      // Set your default username and password
-      const defaultUsername = "admin";
-      const defaultPassword = "password";
-  
-      if (username === defaultUsername && password === defaultPassword) {
+      if (username === defaultUsername && password === defaultPassword || username === storedUsername && password === storedPassword) {
         alert("Login successful!");
         window.location.href = "index.html"; // Redirect to the main app page after successful login.
       } else {

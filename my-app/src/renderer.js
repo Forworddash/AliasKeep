252add2276766631963b3f5714b5382c35896cb3
samuel <samuel.baker@rack-a-tiers.com>
2023-08-28 22:30:21 -0700

    - updated current data storage to array

--- my-app/src/renderer.js
+++ my-app/src/renderer.js
@@ -17,18 +17,32 @@ document.addEventListener("DOMContentLoaded", function () {
         phoneNumber,
         dob,
       };
+
+      // load existing user data array from local storage
+      const existingUserDataArray = JSON.parse(localStorage.getItem("userDataArray")) || [];
   
-      // save user data to local storage
-      localStorage.setItem("userdata", JSON.stringify(userData));
+      // add new user data to array
+      existingUserDataArray.push(userData);
+       // save the updated array back to local storage
+      localStorage.setItem("userdata", JSON.stringify(existingUserDataArray));
       alert("User data saved successfully!");
+
+
+
+      // // save user data to local storage
+      // localStorage.setItem("userdata", JSON.stringify(userData));
+      // alert("User data saved successfully!");
   
       setTimeout(() => {
         location.reload(); // reload page to clear input fields
       }, 100);
     });
-  
+
     // load user data from local storage if available
-    const savedUserData = localStorage.getItem("userdata");
+    const savedUserDataArray = JSON.parse(localStorage.getItem("userdata")) || [];
+  
+    // // load user data from local storage if available
+    // const savedUserData = localStorage.getItem("userdata");
     if (savedUserData) {
       const userData = JSON.parse(savedUserData);
       console.log(userData);
@@ -39,23 +53,32 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("phone-number-input").value = userData.phoneNumber;
       document.getElementById("dob-input").value = userData.dob;
   
-      // display user data entry in the 'database tab'
-      const databaseTable = document.getElementById("tab-2");
-  
-      // create user data entry HTML
-      const userDataEntry = document.createElement("div");
-      userDataEntry.classList.add("user-data-entry");
-  
-      // create HTML structures for the entry
-      userDataEntry.innerHTML = `
-          <h3 class="title is-4">${userData.fullName}</h3>
-          <p>Email: ${userData.email}</p>
-          <p>Phone: ${userData.phoneNumber}</p>
-          <p>Date of Birth: ${userData.dob}</p>
-      `;
-  
-      // append entry to the container
-      databaseTable.appendChild(userDataEntry);
+      const databaseTab = document.querySelector('.tabs li:nth-child(2)');
+
+      databaseTab.addEventListener('click', function() {
+        const savedUserData = localStorage.getItem("userdata");
+        if (savedUserData) {
+          const userData = JSON.parse(savedUserData);
+
+          // display user data entry in the 'database' tab
+          const databaseTab = document.getElementById('tab-2');
+          const existingUserDataEntry = databaseTab.querySelector('.user-data-entry');
+
+          if (!existingUserDataEntry) {
+            const userDataEntry = document.createElement('div');
+            userDataEntry.classList.add('user-data-entry');
+
+            userDataEntry.innerHTML = `
+              <h3 class="title is-4">${userData.fullName}</h3>
+              <p>Email: ${userData.email}</p>
+              <p>Phone: ${userData.phoneNumber}</p>
+              <p>Date of Birth: ${userData.dob}</p>
+            `;
+
+            databaseTab.appendChild(userDataEntry);
+          }
+        }    
+      });
     }
   });
   
